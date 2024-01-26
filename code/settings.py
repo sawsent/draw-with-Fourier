39b593ef7cf187492dfd34d7c3300bde3e1082c1
Vicente Figueiredo <146676109+sawsent@users.noreply.github.com>
2024-01-26 22:07:48 +0000

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -11,8 +11,8 @@ CVS_OFFSET_Y = 0
 PIXEL_SIZE = 3
 
 # drawer settings
-MAX_VECTORS = 0
-DEFAULT_SIMULATION_SPEED = 0.05 # 0,5 - 2 works best for arrow.speed's of 1-10!
+MAX_VECTORS = 0 # 0 means infinite
+DEFAULT_SIMULATION_SPEED = 0.05 # seconds per second
 POINT_RADIUS = 1
 POINT_COLOR = (0,255,255) # pygame compatible colors (rgb: tuple, hex: str ("#FFFFFF"), some base colors like 'red', 'green', 'white', ...)
 DRAW_CENTER = False
@@ -22,7 +22,7 @@ SCALER = 1 # choose to scale every Arrow
 BACKGROUND_COLOR = 'black'
 DRAW_ARROWS = True
 ARROW_WIDTH = 2
-AUTO_COLORS = True # will cycle through COLORS for every Arrow added
-COLORS = COLORS = [(255,0,0,10), (0,255,0,10), (0,0,255,10), (255, 255, 0, 10), (0,255,255,10), (255,255,255,0)]
+AUTO_COLORS = True # will cycle through COLORS for every Arrow(vector) added in the drawer - VERY RECCOMENDED
+COLORS = [(255,0,0,10), (0,255,0,10), (0,0,255,10), (255, 255, 0, 10), (0,255,255,10), (255,255,255,0)]
 
 i = complex(0, 1)
